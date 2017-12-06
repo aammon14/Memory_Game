eb1aa252ad24283b8930c6bf6da5218652b7b13b
Andrew Ammon <andrewammon@Andrews-MBP.home>
2017-12-06 09:01:06 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,21 +1,50 @@
-var cards = ["queen" , "queen" , "king" , "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank:  "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 
 var cardsInPlay = [];
-cardsInPlay.push(cards[0]);
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You've found a match");
+		alert("You've found a match");
 	} else {
-		console.log("Sorry, try again.")
+		alert("Sorry, try again.");
 	}
 };
 
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank)
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
 	checkForMatch ();
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+		cardsInPlay = [];
+	}
 };
 
 flipCard(0);
 flipCard(1);
+
 
